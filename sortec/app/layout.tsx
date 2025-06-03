8f377d7950e2ebb82c0b9adb5bd31070af8e6255
verdugox <luisandresverdugox@gmail.com>
2025-06-03 12:12:15 -0500

    feat:Add configuration for Cloudflare Web Analitycs v2

--- sortec/app/layout.tsx
+++ sortec/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import Head from "next/head"; // 👈 Importamos Head para las metaetiquetas manuales
 import "./globals.css";
 
 const geistSans = Geist({
